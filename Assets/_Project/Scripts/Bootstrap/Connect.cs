176d1d301e8046ec8fc365beb793f3d8745bf652
Lucas <devlucasteles@gmail.com>
2025-05-18 08:33:47 -0300

    feat: :sparkles: funciona, mas n tá bonito

--- Assets/_Project/Scripts/Bootstrap/Connect.cs
+++ Assets/_Project/Scripts/Bootstrap/Connect.cs
@@ -1,8 +1,22 @@
 using Unity.Netcode;
 using UnityEngine;
 
-public class Connect : MonoBehaviour
+public class NetworkPlayerSpawner : NetworkBehaviour
 {
+    [SerializeField]
+    private GameObject pistolPrefab;
+
+    [SerializeField]
+    private GameObject sniperPrefab;
+
+    [SerializeField]
+    private GameObject swordPrefab;
+
+    private void Awake()
+    {
+        Debug.Log("Awake - NetworkPlayerSpawner");
+    }
+
     void Start()
     {
         int gameMode = PlayerPrefs.GetInt("GameMode", 0);
@@ -20,6 +34,55 @@ public class Connect : MonoBehaviour
         else
         {
             Debug.Log("Game mode not set. Please select a game mode.");
+            return;
         }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        Debug.Log("OnNetworkSpawn - NetworkPlayerSpawner");
+        SpawnPlayerServerRpc();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    void SpawnPlayerServerRpc(ServerRpcParams rpcParams = default)
+    {
+        ulong senderClientId = rpcParams.Receive.SenderClientId;
+        Debug.Log($"SpawnPlayerServerRpc - clientId: {senderClientId}");
+
+        int selectedCharacter = PlayerPrefs.GetInt("SelectedCharacter", -1);
+        if (selectedCharacter == -1)
+        {
+            Debug.Log("No character selected. Please select a character.");
+            return;
+        }
+
+        SpawnPlayerOnClients(senderClientId, selectedCharacter);
+    }
+
+    void SpawnPlayerOnClients(ulong clientId, int selectedCharacter)
+    {
+        Debug.Log($"SpawnPlayerOnClients - clientId: {clientId}");
+
+        GameObject playerPrefab = null;
+        switch (selectedCharacter)
+        {
+            case 0:
+                playerPrefab = swordPrefab;
+                break;
+            case 1:
+                playerPrefab = pistolPrefab;
+                break;
+            case 2:
+                playerPrefab = sniperPrefab;
+                break;
+            default:
+                Debug.LogError("Invalid character selection.");
+                return;
+        }
+
+        GameObject player = Instantiate(playerPrefab);
+        var playerNetworkObject = player.GetComponent<NetworkObject>();
+        playerNetworkObject.SpawnAsPlayerObject(clientId);
     }
 }
